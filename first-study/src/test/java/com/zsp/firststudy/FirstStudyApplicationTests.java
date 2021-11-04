d892d3dbf1774124801f06d166266dcfbe9571df
xmzsp <58882553+270520006@users.noreply.github.com>
2021-11-04 17:17:36 +0800

    对sofa-rpc进行了了解写了demo
    对sofa-rpc进行了了解写了demo

---
+++ first-study/src/test/java/com/zsp/firststudy/FirstStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zsp.firststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
